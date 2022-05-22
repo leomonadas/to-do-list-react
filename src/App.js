823f0a8e29d2353dc0e35bab836efddb2e9d9a11
Leonardo <leonardokennedyalvesdelima@gmail.com>
2022-05-22 12:34:13 -0300

    Finalização da aplicação

--- src/App.js
+++ src/App.js
@@ -1,16 +1,55 @@
 import React from "react";
+import { useState } from "react";
 import Navbar from "./components/Navbar";
 import Tasklist from "./components/Tasklist";
 import "./assets/styles/global.css";
 
+let idAcc = 0;
+const generateId = () => {
+  idAcc = idAcc + 1;
+  return idAcc;
+}
+
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (title, state) => {
+    const newTask = {
+      id : generateId(),
+      title,
+      state
+    };
+
+  setTasks((existingTasks) => {
+    return [...existingTasks, newTask];
+  });
+};
+
+  const updateTask = (id, title, state) => {
+    setTasks((existingTasks) => {
+      return existingTasks.map((task) => {
+        if(task.id === id) {
+          return {...task, title, state};
+        } else {
+          return task;
+        }
+      });
+    });
+  };
+
+  const deleteTask = (id) => {
+    setTasks((existingTasks) => {
+      return existingTasks.filter((task) => task.id !== id);
+    })
+  }
+
   return (
     <div className="App">
       <Navbar />
       <div className="container">
-        <Tasklist title="A FAZER" />
-        <Tasklist title="FAZENDO" />
-        <Tasklist title="FEITA" />
+        <Tasklist title="Pendente" onAddTask={addTask} taskState="Pendente" tasks={tasks.filter((t)=>t.state === "Pendente")} onTaskUpdate={updateTask} onDeleteTask={deleteTask} />
+        <Tasklist title="Fazendo" onAddTask={addTask} taskState="Fazendo" tasks={tasks.filter((t)=>t.state === "Fazendo")} onTaskUpdate={updateTask} onDeleteTask={deleteTask} />
+        <Tasklist title="Completa" onAddTask={addTask} taskState="Completa" tasks={tasks.filter((t)=>t.state === "Completa")} onTaskUpdate={updateTask} onDeleteTask={deleteTask} />
       </div>
     </div>
   );
--- src/components/Tasklist/index.js
+++ src/components/Tasklist/index.js
@@ -1,20 +1,35 @@
 import React from "react";
-import { useState } from "react";
 import PropTypes from "prop-types";
 import "./style.css";
+import TaskItem from "../TaskItem/TaskItem";
+import plusIcon from '../../images/plus-icon.svg'
 
-export default function Tasklist(props) {
-    const [count, setcount] = useState();
+export default function Tasklist({ title, taskState, onAddTask, tasks, onTaskUpdate, onDeleteTask }) {
+    const addTask = () => {
+        onAddTask("Nova Tarefa", taskState);
+    }
 
     return(
         <div className="tasklist">
-            <div className="title">{props.title}</div>
-            <div className="content"></div>
+            <div className="title">{title}</div>
+            <div className="content">
+                {tasks.map((task) => {
+                    return(
+                        <TaskItem key={task.id} id={task.id} title={task.title} taskState={task.state} onTaskUpdate={onTaskUpdate} onDeleteTask={onDeleteTask} />
+                    );
+                })}
+                { tasks.length === 0 && <div className="empty-list">Lista vazia</div>}
+            </div>
+            <button onClick={addTask} className="btn">
+                <img src={plusIcon} alt="plus" />
+                Adicionar Tarefa
+            </button>
         </div>
     )
 }
 
 Tasklist.propTypes = {
-    title: PropTypes.string,
-
+    title: PropTypes.string.isRequired,
+    onAddTask: PropTypes.func.isRequired,
+    tasks: PropTypes.array.isRequired
 }
